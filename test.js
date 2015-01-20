fc8e233cf542abbeee511798b6a1e9ec66ed93ad
Trever Faden <treverfaden@gmail.com>
2015-01-20 00:18:49 -0500

    Moved off of Ronin. New Uglify Package

---
+++ test.js
@@ -0,0 +1,8 @@
+var uglifier = require('./lib/main.js'),
+	fs = require('fs');
+
+uglifier.uglify("<style>.demo_class#andID{color:red}</style><div class='demo_class' id='andID'>Welcome to HTML Uglifier</div>", function(e,r){
+	if (e) return console.log(e);
+	console.log("MAP:", r.map);
+	return console.log("HTML:", r.html);
+});
