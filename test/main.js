5bccfcc2b32ae241734f1e83117861cd495f1173
motdotla <mot@mot.la>
2015-04-04 04:24:57 -0400

    Rewrite CSS

--- test/main.js
+++ test/main.js
@@ -23,34 +23,78 @@ describe('Uglify', function() {
     });
   });
 
-  describe('#styles', function() {
-    it('returns 2 style elements', function() {
-      var html = '<style>.some-css{};</style><style>.other-css{};</style>';
-      var results = Uglify.styles(html);
-      assert.equal(results.length, 2);
+  describe('#rewriteCss', function() {
+    it('rewrites an id given lookups', function() {
+      var lookups = { 'id=abe': 'xz' };
+      var html = '<style>#abe{ color: red; }</style>';
+      var $ = cheerio.load(html);
+      var results = Uglify.rewriteCss($, lookups).html();
+      assert.equal(results, '<style>#xz{color:red;}</style>');
     });
-  });
+    it('does not rewrite an id given no lookups', function() {
+      var lookups = { };
+      var html = '<style>#abe{ color: red; }</style>';
+      var $ = cheerio.load(html);
+      var results = Uglify.rewriteCss($, lookups).html();
+      assert.equal(results, '<style>#abe{color:red;}</style>');
+    });
+    it('rewrites a label given lookups', function() {
+      var lookups = { 'id=email': 'ab' };
+      var html = '<style>label[for=email]{ color: blue; }</style>';
+      var $ = cheerio.load(html);
+      var results = Uglify.rewriteCss($, lookups).html();
+      assert.equal(results, '<style>label[for=ab]{color:blue;}</style>');
+    });
+    it('does not rewrite a label given no lookups', function() {
+      var lookups = {};
+      var html = '<style>label[for=email]{ color: blue; }</style>';
+      var $ = cheerio.load(html);
+      var results = Uglify.rewriteCss($, lookups).html();
+      assert.equal(results, '<style>label[for=email]{color:blue;}</style>');
+    });
+    it('rewrites a label with parentheses given lookups', function() {
+      var lookups = { 'id=email': 'ab' };
+      var html = '<style>label[for="email"]{ color: blue; }</style>';
+      var $ = cheerio.load(html);
+      var results = Uglify.rewriteCss($, lookups).html();
+      assert.equal(results, '<style>label[for="ab"]{color:blue;}</style>');
+    });
+    it('rewrites an id= given lookups', function() {
+      var lookups = { 'id=email': 'ab' };
+      var html = '<style>label[id=email]{ color: blue; }</style>';
+      var $ = cheerio.load(html);
+      var results = Uglify.rewriteCss($, lookups).html();
+      assert.equal(results, '<style>label[id=ab]{color:blue;}</style>');
+    });
+    it('rewrites an id= without parentheses given lookups', function() {
+      var lookups = { 'id=email': 'ab' };
+      var html = '<style>label[id="email"]{ color: blue; }</style>';
+      var $ = cheerio.load(html);
+      var results = Uglify.rewriteCss($, lookups).html();
+      assert.equal(results, '<style>label[id="ab"]{color:blue;}</style>');
+    });
+    it('rewrites a class given lookups', function() {
+      var lookups = { 'class=email': 'ab' };
+      var html = '<style>label.email{ color: blue; }</style>';
+      var $ = cheerio.load(html);
+      var results = Uglify.rewriteCss($, lookups).html();
+      assert.equal(results, '<style>label.ab{color:blue;}</style>');
+    });
+    it('rewrites css media queries', function() {
+      var lookups = { 'id=abe': 'wz' };
 
-  describe('#elements', function() {
-    it('returns one element', function() {
-      var html = '<h1>Header 1</h1>';
-      var results = Uglify.elements(html);
-      assert.equal(results.length, 1);
+      var html = '<style>@media screen and (max-width: 300px) { #abe{ color: red; } }</style>';
+      var $ = cheerio.load(html);
+      var results = Uglify.rewriteCss($, lookups).html();
+      assert.equal(results, '<style>@media screen and (max-width: 300px){#wz{color:red;}}</style>');
     });
-    it('returns two elements', function() {
-      var html = '<h1>Header 1</h1><p>Paragraph</p>';
-      var results = Uglify.elements(html);
-      assert.equal(results.length, 2);
-    });
-    it('returns two elements when nested', function() {
-      var html = '<h1>Header <strong>1</strong></h1>';
-      var results = Uglify.elements(html);
-      assert.equal(results.length, 2);
-    });
-    it('returns three elements when nested', function() {
-      var html = '<h1>Header <strong><span>1</span></strong></h1>';
-      var results = Uglify.elements(html);
-      assert.equal(results.length, 3);
+    it('rewrites nested css media queries', function() {
+      var lookups = { 'id=abe': 'wz' };
+
+      var html = '<style>@media { @media screen and (max-width: 300px) { #abe{ color: red; } } }</style>';
+      var $ = cheerio.load(html);
+      var results = Uglify.rewriteCss($, lookups).html();
+      assert.equal(results, '<style>@media {@media screen and (max-width: 300px){#wz{color:red;}}}</style>');
     });
   });
 
@@ -84,6 +128,30 @@ describe('Uglify', function() {
       var $ = cheerio.load(html);
       var results = Uglify.rewriteElements($).html();
       assert.equal(results, '<h1 id="xz">Header <strong id="wk"><span id="en">1</span></strong></h1><label for="km">Something</label><label for="dj">null</label><div class="yw qr">Some Classes</div>');
+    });
+    it('rewrites ids and labels to match when matching', function() {
+      var html = '<h1 id="header">Header</h1><label for="header">Something</label><label for="header">Other</label>';
+      var $ = cheerio.load(html);
+      var results = Uglify.rewriteElements($).html();
+      assert.equal(results, '<h1 id="xz">Header</h1><label for="xz">Something</label><label for="xz">Other</label>');
+    });
+    it('rewrites multiple uses of the same class to the correct value', function() {
+      var html = '<h1 class="header">Header</h1><label class="header">Something</label><div class="header">Other</div>';
+      var $ = cheerio.load(html);
+      var results = Uglify.rewriteElements($).html();
+      assert.equal(results, '<h1 class="xz">Header</h1><label class="xz">Something</label><div class="xz">Other</div>');
+    });
+    it('rewrites multiple uses of the same class to the correct value', function() {
+      var html = '<h1 class="header">Header</h1><label class="header">Something</label><div class="other">Other</div><div class="again">Again</div>';
+      var $ = cheerio.load(html);
+      var results = Uglify.rewriteElements($).html();
+      assert.equal(results, '<h1 class="xz">Header</h1><label class="xz">Something</label><div class="wk">Other</div><div class="en">Again</div>');
+    });
+    it('rewrites other class combinations', function() {
+      var html = '<h1 class="header other">Header</h1><label class="header">Something</label><div class="other">Other</div><div class="again">Again</div>';
+      var $ = cheerio.load(html);
+      var results = Uglify.rewriteElements($).html();
+      assert.equal(results, '<h1 class="xz wk">Header</h1><label class="xz">Something</label><div class="wk">Other</div><div class="en">Again</div>');
     });
   });
 
